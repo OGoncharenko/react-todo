558957a85e2de59e66ccbef829f63925ee789853
OGoncharenko <olyas.goncharenko@gmail.com>
2024-02-26 16:37:13 -0800

    Week 1, lesson_1_1

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const todoList = [
+    {
+      title: 'Complete the assignment 1',
+      id: 1,
+    },
+    {
+      title: 'Complete the assignment 2',
+      id: 2,
+    },
+    {
+      title: 'Complete the assignment 3',
+      id: 3,
+    }
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite! + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="header">Todo List</h1>
+      <ul>
+        {
+          todoList.map(function(item) {
+          return <li key={item.id}>{item.title}</li>;
+          })
+        }
+      </ul>
     </>
   )
 }
 
-export default App
+export default App;
